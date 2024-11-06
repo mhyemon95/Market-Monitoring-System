b5838a69e7871db05ec70b6347a1823ae0f47e61
Muhtasim <muhtasimrahman810@gmail.com>
2024-11-07 00:56:03 +0600

    Implement User CRUD

---
+++ apps/Users/urls.py
@@ -0,0 +1,34 @@
+from django.urls import path
+from .views import (
+    register_admin,
+    register_farmer,
+    register_seller,
+    register_whole_seller,
+    activate,
+    user_login,
+    user_logout,
+    user_profile,
+    update_password,
+    reset_password,
+    forgot_password,
+    forgot_password_active_url,
+)
+
+urlpatterns = [
+    path("register/farmer/", register_farmer, name="register_farmer"),
+    path("register/whole_seller/", register_whole_seller, name="register_whole_seller"),
+    path("register/seller/", register_seller, name="register_seller"),
+    path("register/admin/", register_admin, name="register_admin"),
+    path("activate/<uidb64>/<token>/", activate, name="activate"),
+    path("login/", user_login, name="login"),
+    path("logout/", user_logout, name="logout"),
+    path("profile/", user_profile, name="profile"),
+    path("update/password/", update_password, name="update_password"),
+    path("forgot-password/", forgot_password, name="forgot_password"),
+    path(
+        "forgot-password/<uid>/<token>",
+        forgot_password_active_url,
+        name="forgot_password_url",
+    ),
+    path("reset-password/", reset_password, name="reset_password"),
+]
